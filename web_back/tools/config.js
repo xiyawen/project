fb1fcbb8628b3a5c9896d45744c4b26f0c36f602
xi <617426726@qq.com>
2019-12-17 14:37:59 +0800

    用户信息功能添加

--- web_back/tools/config.js
+++ web_back/tools/config.js
@@ -2,4 +2,11 @@ var config = "http://localhost:8000/";
 
 var LOGIN = config + "admin/login";
 var LOGOUT = config + "admin/logout";
+// 用户信息接口
+var GETUSER = config + "admin/getuser"
+
 var COUNT = config + "admin/article_count";
+//获取用户信息
+var USERINFO_GET = config + "admin/userinfo_get";
+
+
--- web_back/tools/user.js
+++ web_back/tools/user.js
@@ -30,5 +30,62 @@ var user = {
             }
         })
     },
-
+    // 用户信息
+    getUser: function (options) {
+        $.ajax({
+            url: GETUSER,
+            success: function (res) {
+                console.log(res);
+                options.success(res);
+            }
+        })
+    },
+    // 文章数量
+    article_count: function (options) {
+        $.ajax({
+            url: COUNT,
+            success: function (res) {
+                // console.log(res);
+                options.success(res)
+            }
+        })
+    },
+    // 评论数量
+    comment_count: function (options) {
+        $.ajax({
+            url: "http://localhost:8000/admin/comment_count",
+            success: function (res) {
+                console.log(res);
+                options.success(res)
+            }
+        })
+    },
+    // 获取用户信息
+    userinfo_get: function (options) {
+        $.ajax({
+            url: USERINFO_GET,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success(res)
+                }
+            }
+        })
+    },
+    // 用户信息编辑
+    userinfo_edit: function (options) {
+        // 声明formDate
+        var fd = new FormData($("form")[0]);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8000/admin/userinfo_edit",
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success();
+                }
+            }
+        })
+    }
 }
